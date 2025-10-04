eea4211a99569c04d5cb4461d0a08b2824d788fe
cadenceplays <cliao@eastsideprep.org>
2025-10-03 19:59:43 -0700

    added leo chang to volunteer awards page
    yo someone give me an award fr

--- src/pages/VolunteerAwards.jsx
+++ src/pages/VolunteerAwards.jsx
@@ -11,11 +11,11 @@ const VolunteerAwards = () => {
 
             <div className="volunteerawards_content_container">
                 <div className="volunteerawards_container">
-                    
+
                     <div className="volunteerawards_photo">
-                        <img 
-                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Flucas%20zhang.jpg?alt=media&token=bcd6a410-9a85-40bf-9082-684ad434f801" 
-                            alt="Lucas Zhang" 
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Flucas%20zhang.jpg?alt=media&token=bcd6a410-9a85-40bf-9082-684ad434f801"
+                            alt="Lucas Zhang"
                         />
                     </div>
 
@@ -30,9 +30,9 @@ const VolunteerAwards = () => {
             <div className="volunteerawards_content_container">
                 <div className="volunteerawards_container">
                     <div className="volunteerawards_photo">
-                        <img 
-                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Fsaroja%20subramanian.jpg?alt=media&token=f4c25261-5154-4df6-9c59-07cd0e4966a5" 
-                            alt="Saroja Subramanian" 
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Fsaroja%20subramanian.jpg?alt=media&token=f4c25261-5154-4df6-9c59-07cd0e4966a5"
+                            alt="Saroja Subramanian"
                         />
                     </div>
 
@@ -47,15 +47,32 @@ const VolunteerAwards = () => {
             <div className="volunteerawards_content_container">
                 <div className="volunteerawards_container">
                     <div className="volunteerawards_photo">
-                        <img 
-                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Fnishka%20batra.jpg?alt=media&token=bc13b4c6-9c12-4d03-80d9-7b5700fb3be6" 
-                            alt="Nishka Batra" 
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Fnishka%20batra.jpg?alt=media&token=bc13b4c6-9c12-4d03-80d9-7b5700fb3be6"
+                            alt="Nishka Batra"
                         />
                     </div>
 
                     <div className="volunteerawards_content">
                         <h2 className="volunteerawards_name">Most Improved Award</h2>
                         <p className="volunteerawards_extra">Nishka Batra</p>
+                        <p className="volunteerawards_extra"></p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="volunteerawards_content_container">
+                <div className="volunteerawards_container">
+                    <div className="volunteerawards_photo">
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/our_team%2F2025%20leaders%2Fc0c2534404e3d64cd51a342e4e0bc1b1.JPG?alt=media&token=c8432342-c00c-47a2-b349-4575dd7b8a6b"
+                            alt="Leo Chang"
+                        />
+                    </div>
+
+                    <div className="volunteerawards_content">
+                        <h2 className="volunteerawards_name">Volunteer Inspiration Award</h2>
+                        <p className="volunteerawards_extra">Leo Chang</p>
                         <p className="volunteerawards_extra"></p>
                     </div>
                 </div>
