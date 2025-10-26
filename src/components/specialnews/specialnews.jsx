e352e9c227a85a85f877f63c71ee7b4fe2fe9116
cadenceplays <cliao@eastsideprep.org>
2025-10-26 16:47:34 -0700

    updates during meeting
    no doc

--- src/components/specialnews/specialnews.jsx
+++ src/components/specialnews/specialnews.jsx
@@ -50,9 +50,9 @@ const Specialnews = () => {
                     <div className="specialnewslist-container">
 
 
-                        {/*<div className="specialnewslist-cell-empty"></div>*/}
+                        <div className="specialnewslist-cell-empty"></div>
 
-                        <div className="specialnewslist-cell">
+                        {/*<div className="specialnewslist-cell">
                             <div className="specialnewslist-button-date">October 25th</div>
                             <div className="specialnewslist-button-title">
                                 <Link to="/upcomingweekly" state={{ data: dataContent }}>
@@ -78,7 +78,7 @@ const Specialnews = () => {
                                     <img src="https://firebasestorage.googleapis.com/v0/b/internationalbuddy2018.appspot.com/o/event_upcomings%2Fhalloween%20party%20poster.jpg?alt=media&token=7fdef3bb-13f8-4c07-a3da-096bed71d051" width="100%" alt="featured" />
                                 </Link>
                             </div>
-                        </div>
+                        </div>*/}
 
                         <div className="specialnewslist-cell">
                             <div className="specialnewslist-button-date">October 26th</div>
@@ -107,6 +107,8 @@ const Specialnews = () => {
                                 </Link>
                             </div>
                         </div>
+
+                        <div className="specialnewslist-cell-empty"></div>
 
                     </div>
                 </div>
